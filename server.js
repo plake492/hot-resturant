27f02a0cd1207de35551cdec5b3dd5d44d420347
plake <plake492@gmail.com>
2019-08-27 19:19:40 -0400

    it can be deployed

---
+++ server.js
@@ -0,0 +1,95 @@
+// Dependencies
+// =============================================================
+const express = require("express");
+const path = require("path");
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 3000;
+// console.log("Hello!");
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Tables & Waiting List (DATA)
+// =============================================================
+const tables = [
+    {
+      id: 1,
+      name: "Ashley Morales",
+      email: "ashley@me.com",
+      phone: 407-407-4077,
+    },
+    {
+        id: 2,
+        name: "Connor Durham",
+        email: "connor@me.com",
+        phone: 222-222-2225,
+    },
+    {
+        id: 3,
+        name: "Patrick Lake",
+        email: "patrick@me.com",
+        phone: 555-555-4555,
+    },
+  ];
+  const waitingList = [
+    {
+        id: 6,
+        name: "Jen Ross",
+        email: "jen@me.com",
+        phone: 333-333-3336,
+    },
+  ];
+  // Routes
+// =============================================================
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
+  
+  app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+  });
+  
+  // Displays all tables
+  app.get("/tables", function(req, res) {
+    return res.json(tables);
+  });
+  
+  // Displays a single character, or returns false
+  app.get("/:tables", function(req, res) {
+    let chosen = req.params.tables;
+  
+    console.log(chosen);
+  
+    for (var i = 0; i < tables.length; i++) {
+      if (chosen === tables[i].id) {
+        return res.json(tables[i]);
+      }
+    }
+  
+    return res.json(false);
+  });
+  
+  // Create New Reservation - takes in JSON input
+  app.post("/reserve", function(req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    let newTable = req.body;
+  
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    newTable.id = newTable.name.replace(/\s+/g, "").toLowerCase();
+  
+    console.log(newTable);
+  
+    tables.push(newTable);
+  
+    res.json(newTable);
+  });
+  
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
